c99091ac77c023a06d8d425f43b2a4762d8902d4
IGOR DOMREV <domrevigor@gmail.com>
2025-10-08 12:43:07 +0300

    updating readme

--- app/components/Visually.jsx
+++ app/components/Visually.jsx
@@ -10,8 +10,8 @@ import {useAnalytics} from '@shopify/hydrogen';
  * @returns {JSX.Element} An empty div.
  */
 export function VisuallyConnect() {
-  useVisuallyConnect();
-  return <div />;
+    useVisuallyConnect();
+    return <div/>;
 }
 
 /**
@@ -19,49 +19,49 @@ export function VisuallyConnect() {
  * Sets up Visually connect event handlers and listeners.
  */
 const useVisuallyConnect = () => {
-  const isLoaded = useIsVisuallyLoaded();
+    const isLoaded = useIsVisuallyLoaded();
 
-  const {shop} = useAnalytics();
-  const {open} = useAside();
-  const cartWithActions = useCart();
+    const {shop} = useAnalytics();
+    const {open} = useAside();
+    const cartWithActions = useCart();
 
-  useEffect(() => {
-    if (!isLoaded) return;
-    window.visually.analyticsProcessingAllowed = () => true; // Change this to false if user declined tracking consent
-    window.visually.visuallyConnect({
-      cartClear: () => cartWithActions.linesRemove(cartWithActions.lines.map(({id}) => id)),
-      addToCart: (variantId, quantity) => cartWithActions.linesAdd([
-        {
-          merchandiseId: `gid://shopify/ProductVariant/${variantId}`,
-          quantity,
-        },
-      ]),
-      cartAddAttributes: (attributes) => cartWithActions.cartAttributesUpdate(attributes),
-      openCartDrawer: () => open('cart'),
-      initialCurrency: shop.currency,
-      customerTags: [], // strings array
-      country: '', // initialize in case you have a country selection - ISO CODE
-      initialLocale: shop.acceptedLanguage.toLocaleLowerCase(),
-    });
-  }, [isLoaded]);
+    useEffect(() => {
+        if (!isLoaded) return;
+        window.visually.analyticsProcessingAllowed = () => true; // Change this to false if user declined tracking consent
+        window.visually.visuallyConnect({
+            cartClear: () => cartWithActions.linesRemove(cartWithActions.lines.map(({id}) => id)),
+            addToCart: (variantId, quantity) => cartWithActions.linesAdd([
+                {
+                    merchandiseId: `gid://shopify/ProductVariant/${variantId}`,
+                    quantity,
+                },
+            ]),
+            cartAddAttributes: (attributes) => cartWithActions.cartAttributesUpdate(attributes),
+            openCartDrawer: () => open('cart'),
+            initialCurrency: shop.currency,
+            customerTags: [], // strings array
+            country: '', // initialize in case you have a country selection - ISO CODE
+            initialLocale: shop.acceptedLanguage.toLocaleLowerCase(),
+        });
+    }, [isLoaded]);
 
-  const transformedCart = transformCart(cartWithActions);
-  useEffect(() => {
-    if (!isLoaded) return;
-    maybe(() => window.visually.onCartChanged(transformedCart));
-  }, [isLoaded, hash(transformedCart)]);
+    const transformedCart = transformCart(cartWithActions);
+    useEffect(() => {
+        if (!isLoaded) return;
+        maybe(() => window.visually.onCartChanged(transformedCart));
+    }, [isLoaded, hash(transformedCart)]);
 
-  const {pathname} = useLocation();
-  const pageType = getPageType(pathname);
-  useEffect(() => {
-    if (!isLoaded) return;
-    maybe(() => window.visually.pageTypeChanged(pageType));
-  }, [isLoaded, pageType]);
+    const {pathname} = useLocation();
+    const pageType = getPageType(pathname);
+    useEffect(() => {
+        if (!isLoaded) return;
+        maybe(() => window.visually.pageTypeChanged(pageType));
+    }, [isLoaded, pageType]);
 
-  useEffect(() => {
-    if (!isLoaded) return;
-    maybe(() => window.visually.localeChanged(shop?.currency));
-  }, [isLoaded,shop?.currency]);
+    useEffect(() => {
+        if (!isLoaded) return;
+        maybe(() => window.visually.localeChanged(shop?.currency));
+    }, [isLoaded, shop?.currency]);
 
 };
 
@@ -72,48 +72,48 @@ const useVisuallyConnect = () => {
  * @returns {JSX.Element} React fragment with link and script tags.
  */
 export function VisuallySDK({alias, clientKey}) {
-  return (
-    <>
-      <link
-        rel="preconnect"
-        href="https://live.visually-io.com/"
-        crossOrigin="anonymous"
-      />
-      <link rel="dns-prefetch" href="https://live.visually-io.com/" />
-      <link
-        rel="preload"
-        as="script"
-        href={`"https://live.visually-io.com/widgets/vsly-preact.min.js?k=js.${clientKey}&e=2&s=${alias}"`}
-      />
-      <link
-        rel="preload"
-        as="script"
-        href={`https://live.visually-io.com/cf/${alias}.js`}
-      />
-      <link
-        rel="preload"
-        as="script"
-        href="https://live.visually-io.com/v/visually-spa.js"
-      />
-      <script
-        type="text/javascript"
-        src={`https://live.visually-io.com/widgets/vsly-preact.min.js?k=js.${clientKey}&e=2&s=${alias}`}
-      />
-      <script
-        type="text/javascript"
-        src={`https://live.visually-io.com/cf/${alias}.js`}
-      />
-      <script
-        type="text/javascript"
-        src="https://live.visually-io.com/v/visually-spa.js"
-      />
-      <script
-        defer
-        type="text/javascript"
-        src="https://live.visually-io.com/v/visually-a-spa.js"
-      />
-    </>
-  );
+    return (
+        <>
+            <link
+                rel="preconnect"
+                href="https://live.visually-io.com/"
+                crossOrigin="anonymous"
+            />
+            <link rel="dns-prefetch" href="https://live.visually-io.com/"/>
+            <link
+                rel="preload"
+                as="script"
+                href={`"https://live.visually-io.com/widgets/vsly-preact.min.js?k=js.${clientKey}&e=2&s=${alias}"`}
+            />
+            <link
+                rel="preload"
+                as="script"
+                href={`https://live.visually-io.com/cf/${alias}.js`}
+            />
+            <link
+                rel="preload"
+                as="script"
+                href="https://live.visually-io.com/v/visually-spa.js"
+            />
+            <script
+                type="text/javascript"
+                src={`https://live.visually-io.com/widgets/vsly-preact.min.js?k=js.${clientKey}&e=2&s=${alias}`}
+            />
+            <script
+                type="text/javascript"
+                src={`https://live.visually-io.com/cf/${alias}.js`}
+            />
+            <script
+                type="text/javascript"
+                src="https://live.visually-io.com/v/visually-spa.js"
+            />
+            <script
+                defer
+                type="text/javascript"
+                src="https://live.visually-io.com/v/visually-a-spa.js"
+            />
+        </>
+    );
 }
 
 /**
@@ -124,11 +124,11 @@ export function VisuallySDK({alias, clientKey}) {
  * @returns {T|undefined}
  */
 export function maybe(f, def = undefined) {
-  try {
-    return f();
-  } catch {
-    return def;
-  }
+    try {
+        return f();
+    } catch {
+        return def;
+    }
 }
 
 /**
@@ -136,34 +136,35 @@ export function maybe(f, def = undefined) {
  * @param {object} cart Cart object from Shopify's useCart.
  * @returns {CartBase|undefined} Transformed cart object or undefined if cart is not provided.
  */
-function transformCart(cart) {  return cart
-    ? {
-      item_count: maybe(() => cart.lines.reduce((p, c) => p + c.quantity, 0)),
-      attributes: cart?.attributes || [],
-      items: cart.lines.map((l) => ({
-        // selling_plan_allocation:{selling_plan:{name:undefined}}, // add a selling plan if available
-        handle: l.merchandise.product.handle,
-        price: maybe(() => parseFloat(l.cost.totalAmount.amount) * 100), // PRICE IN CENTS
-        product_id: maybe(() =>
-          parseFloat(
-            l.merchandise.product.id.replace('gid://shopify/Product/', ''),
-          ),
-        ),
-        quantity: l.quantity,
-        variant_id: maybe(() =>
-          parseFloat(
-            l.merchandise.id.replace('gid://shopify/ProductVariant/', ''),
-          ),
-        ),
-      })),
-      currency: maybe(() => cart.cost.totalAmount.currencyCode, 0),
-      total_price: maybe(
-        () => parseFloat(cart.cost.totalAmount.amount) * 100, // PRICE IN CENTS
-        0,
-      ),
-      token: maybe(() => cart.id.replace('gid://shopify/Cart/', ''), ''),
-    }
-    : undefined;
+function transformCart(cart) {
+    return cart
+        ? {
+            item_count: maybe(() => cart.lines.reduce((p, c) => p + c.quantity, 0)),
+            attributes: cart?.attributes || [],
+            items: cart.lines.map((l) => ({
+                // selling_plan_allocation:{selling_plan:{name:undefined}}, // add a selling plan if available
+                handle: l.merchandise.product.handle,
+                price: maybe(() => parseFloat(l.cost.totalAmount.amount) * 100), // PRICE IN CENTS
+                product_id: maybe(() =>
+                    parseFloat(
+                        l.merchandise.product.id.replace('gid://shopify/Product/', ''),
+                    ),
+                ),
+                quantity: l.quantity,
+                variant_id: maybe(() =>
+                    parseFloat(
+                        l.merchandise.id.replace('gid://shopify/ProductVariant/', ''),
+                    ),
+                ),
+            })),
+            currency: maybe(() => cart.cost.totalAmount.currencyCode, 0),
+            total_price: maybe(
+                () => parseFloat(cart.cost.totalAmount.amount) * 100, // PRICE IN CENTS
+                0,
+            ),
+            token: maybe(() => cart.id.replace('gid://shopify/Cart/', ''), ''),
+        }
+        : undefined;
 }
 
 /**
@@ -172,18 +173,18 @@ function transformCart(cart) {  return cart
  * @returns {'home'|'product'|'collection'|'other'} The resolved page type.
  */
 function getPageType(pathname) {
-  const pageType = 'other';
+    const pageType = 'other';
 
-  if (pathname === '/') {
-    return 'home';
-  }
-  if (pathname.startsWith('/products/')) {
-    return 'product';
-  }
-  if (pathname.startsWith('/collections/')) {
-    return 'collection';
-  }
-  return pageType;
+    if (pathname === '/') {
+        return 'home';
+    }
+    if (pathname.startsWith('/products/')) {
+        return 'product';
+    }
+    if (pathname.startsWith('/collections/')) {
+        return 'collection';
+    }
+    return pageType;
 }
 
 /**
@@ -192,11 +193,11 @@ function getPageType(pathname) {
  * @returns {object} The transformed variant.
  */
 export const transformVariant = (selected) => {
-  return {
-    id: selected?.id?.replace('gid://shopify/ProductVariant/', ''),
-    price: selected?.price?.amount * 100, // price in cents
-    iq: selected.quantityAvailable,
-  };
+    return {
+        id: selected?.id?.replace('gid://shopify/ProductVariant/', ''),
+        price: selected?.price?.amount * 100, // price in cents
+        iq: selected.quantityAvailable,
+    };
 };
 
 /**
@@ -205,16 +206,16 @@ export const transformVariant = (selected) => {
  * @returns {CurrentProduct|undefined} The transformed product or undefined if failed.
  */
 export function transformProduct(product) {
-  return maybe(() => {
-    const selected = product.selectedOrFirstAvailableVariant;
-    const variants = [selected, ...(product?.adjacentVariants || [])];
-    return {
-      variants: [...variants.map(transformVariant)],
-      id: product.id.replace('gid://shopify/Product/', ''),
-      oos: false,
-      price: selected?.price?.amount * 100, // cents
-    };
-  });
+    return maybe(() => {
+        const selected = product.selectedOrFirstAvailableVariant;
+        const variants = [selected, ...(product?.adjacentVariants || [])];
+        return {
+            variants: [...variants.map(transformVariant)],
+            id: product.id.replace('gid://shopify/Product/', ''),
+            oos: false,
+            price: selected?.price?.amount * 100, // cents
+        };
+    });
 }
 
 /**
@@ -225,41 +226,55 @@ export function transformProduct(product) {
  * notice! make sure that transformProduct and transformVariant take all necessary fields for the object
  */
 export const useVisuallyOnPDPChange = (product, selectedVariant) => {
-  const isLoaded = useIsVisuallyLoaded();
-  const transformedProduct = transformProduct(product);
-  useEffect(() => {
-    if (!isLoaded) return;
-    maybe(() => window.visually.productChanged(transformedProduct));
-  }, [isLoaded, hash(transformedProduct)]);
+    const isLoaded = useIsVisuallyLoaded();
+    const transformedProduct = transformProduct(product);
+    useEffect(() => {
+        if (!isLoaded) return;
+        maybe(() => window.visually.productChanged(transformedProduct));
+    }, [isLoaded, hash(transformedProduct)]);
 
-  const transformedVariant = transformVariant(selectedVariant);
-  useEffect(() => {
-    if (!isLoaded) return;
-    maybe(() => window.visually.variantChanged(transformedVariant));
-  }, [isLoaded, hash(transformedVariant)]);
+    const transformedVariant = transformVariant(selectedVariant);
+    useEffect(() => {
+        if (!isLoaded) return;
+        maybe(() => window.visually.variantChanged(transformedVariant));
+    }, [isLoaded, hash(transformedVariant)]);
 
 };
 
 function hash(obj) {
-  if (!obj) return '';
-  return JSON.stringify(obj, Object.keys(obj).sort());
+    if (!obj) return '';
+    return JSON.stringify(obj, Object.keys(obj).sort());
 }
-const useIsVisuallyLoaded = () => {
-  const [isLoaded, setIsLoaded] = useState(false);
 
-  useEffect(() => {
-    if (typeof window === 'undefined') return;
-    const abortController = new AbortController();
-    const handleVisuallyInit = () => {
-      setIsLoaded(!!window.visually?.visuallyConnect);
-    };
-    window.addEventListener('x-visually-init', handleVisuallyInit, {
-      once: true,
-      signal: abortController.signal,
-    });
-    return () => {
-      abortController.abort();
-    };
-  }, []);
-  return isLoaded;
+const useIsVisuallyLoaded = () => {
+    const [isLoaded, setIsLoaded] = useState(false);
+    awaitCondition(() => !!window.visually?.visuallyConnect, 50, 100).then(() => setIsLoaded(!isLoaded))
+
+    useEffect(() => {
+        if (typeof window === 'undefined') return;
+        const handleVisuallyInit = () => setIsLoaded(!isLoaded);
+        window.addEventListener('x-visually-init', handleVisuallyInit);
+        return () => window.removeEventListener('x-visually-init', handleVisuallyInit);
+    }, []);
+    return isLoaded;
 };
+
+export const awaitCondition = (
+    condition,
+    timeout ,
+    retries ,
+) => new Promise((resolve, reject) => {
+    if (condition()) {
+        resolve(0)
+        return
+    }
+    if (retries === 0) {
+        reject(0)
+        return
+    }
+    setTimeout(() => {
+        awaitCondition(condition, timeout, retries - 1)
+            .then(resolve)
+            .catch(reject)
+    }, timeout)
+});
